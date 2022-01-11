92883d7f3186d04dd3fdf3c11dc52d14c671f8b4
daniela <daniela_056@hotmail.com>
2022-01-10 19:07:27 -0700

    Debug webdriver file

--- commands.py
+++ commands.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.web driver.common.keys import Keys
+from selenium.webdriver.chrome.webdriver import WebDriver
+import time
 
 # Chrome
 driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+driver.get("https://www.google.com")
+print(driver.title)
+print(driver.current_url)
+
+driver.find_element_by_xpath('//*[@id="gb"]/div/div[1]/div/div[1]/a').click()
+time.sleep(3)
+driver.close()
