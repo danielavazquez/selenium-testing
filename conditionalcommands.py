e3a6fa002b9683522d9cd5729e6287c6d6782aab
daniela <daniela_056@hotmail.com>
2022-01-12 14:53:46 -0700

    Finishing conditional statements

--- conditionalcommands.py
+++ conditionalcommands.py
@@ -12,3 +12,15 @@ print(user_ele.is_enabled())  # also returns true/false based upon status of ele
 pw_ele = driver.find_element_by_name("password")
 print(pw_ele.is_displayed())
 print(pw_ele.is_enabled())
+
+user_ele.send_keys("mercury")
+pw_ele.send_keys("mercury")
+
+driver.find_element_by_name("login").click()
+
+roundtrip_radio = driver.find_element_by_css_selector("input[value=roundtrip]")
+print("status of round trip radio button", roundtrip_radio.is_selected()) #print status of radio button round trip
+
+onetrip_radio = driver.find_element_by_css_selector("input[value=oneway]")
+
+print("status of one way radio button", onetrip_radio.is_selected())
