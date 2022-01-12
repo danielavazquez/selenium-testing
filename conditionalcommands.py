9ee6e02b4de16e1800c219a0fc70211f364ed83f
daniela <daniela_056@hotmail.com>
2022-01-12 12:46:02 -0700

    Conditional commands

--- conditionalcommands.py
+++ conditionalcommands.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver=webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 
-driver.get("https://newtours.demoaut.com")
+driver.get("https://www.discover.com/")
+
+user_ele = driver.find_element_by_name("userID")
+print(user_ele.is_displayed())  # returns true/false based upon status of element
+print(user_ele.is_enabled())  # also returns true/false based upon status of element
+
+pw_ele = driver.find_element_by_name("password")
+print(pw_ele.is_displayed())
+print(pw_ele.is_enabled())
