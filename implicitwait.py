08dbc72627ca834e1934aba744512fcf9093c65a
daniela <daniela_056@hotmail.com>
2022-01-14 19:41:27 -0700

    Implicit wait

---
+++ implicitwait.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+
+driver.get("http://newtours.demoaut.com/")  # opening URL takes some time
+
+# wait some time here
+driver.implicitly_wait(10)  # seconds specify only 1 time
+assert "Welcome: Mercury Tours" in driver.title
+
+driver.find_element_by_name("userName").send_keys("mercury")
+driver.find_element_by_name("password").send_keys("mercury")
+driver.find_element_by_name("login").click()
