19223f6bf836d5f59657b87613fcbeceec56bfab
daniela <daniela_056@hotmail.com>
2022-01-14 20:52:23 -0700

    Explicit wait

--- explicitwait.py
+++ explicitwait.py
@@ -10,6 +10,16 @@ driver.maximize_window()
 
 driver.get("https://www.expedia.com/")
 driver.find_element_by_id("tab-flight-tab-hp").click() # click on the flights button
+driver.find_element_by_id("flight-origin-hp-flight").send_keys("SFO") # origin
 
 time.sleep(2) # python syntax
 
+driver.find_element_by_id("flight-destination-hp-flight").send_keys("NYC") # destination
+
+driver.find_element_by_id("flight-destination-hp-flight").clear()
+driver.find_element_by_id("flight-departing-hp-flight").send_keys("12/10/2018")
+
+driver.find_element_by_id("flight-returning-hp-flight").clear()
+driver.find_element_by_id("flight-returning-hp-flight").send_keys("15/10/2018")
+
+driver.find_element_by_xpath('//*[@id='gcw-flights-form-hp-flight']/div[7]/label/button').click()
